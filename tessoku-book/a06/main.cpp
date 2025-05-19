8f56e9d92e2bf516e69c9001ae6d4dce0e36e64e
HukukaichOu <kouki0802.ao@gmail.com>
2025-05-20 00:04:46 +0900

    a07をACした

--- tessoku-book/a06/main.cpp
+++ tessoku-book/a06/main.cpp
@@ -5,13 +5,21 @@ using namespace std;
 int main() {
     int n, q;
     cin >> n >> q;
-
-    vector<int> a(n), l(q), r(q);
+    vector<int> a(n + 1, 0), l(q), r(q);
     for (int i = 0; i < n; i++) {
-        cin >> a[i];
+        cin >> a[i + 1];
     }
+    for (int j = 0; j < q; j++) {
+        cin >> l[j] >> r[j];
+    }
+
+    vector<int> s(n + 1, 0);
+    for (int i = 0; i < n; i++) {
+        s[i + 1] = s[i] + a[i + 1];
+    }
+
     for (int i = 0; i < q; i++) {
-        cin >> l[i] >> r[i];
+        cout << s[r[i]] - s[l[i] - 1] << endl;
     }
 
     return 0;
